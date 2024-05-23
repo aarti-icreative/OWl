9cedd8418eb57fe69ad8fc7d467ad8555222e746
aarti-icreative <aarti@icreativetechnolabs.com>
2024-05-23 18:55:10 +0530

    [MOD]fetched data from model

--- owl_to_do_list/static/src/components/todo_list/todo_list.js
+++ owl_to_do_list/static/src/components/todo_list/todo_list.js
@@ -1,11 +1,47 @@
 /** @odoo-module **/
 
-const { Component, useState } = owl;
+const { Component, useState, onWillStart } = owl;
 import { registry } from '@web/core/registry';
+import { useService } from "@web/core/utils/hooks";
+
 
 export class OwlTodoList extends Component {
     setup() {
-        this.state = useState({ value: 1 })
+        this.state = useState({
+            task: { name: "", color: "#FF0000", completed: false },
+            taskList: [],
+            isEdit: false,
+            activeId: false,
+        })
+        this.orm = useService("orm");
+        this.model = 'todo.list'
+
+        onWillStart(async () => {
+            await this.getAllTasks()
+        })
+    }
+
+    async getAllTasks() {
+        this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "color", "completed"])
+    }
+
+    addTask() {
+
+    }
+
+    editTask() {
+
+    }
+
+    async saveTask() {
+        await this.orm.create(this.model,
+            [{
+                name: this.state.task.name,
+                color: name: this.state.task.color,
+                completed: name: this.state.task.completed
+            }]
+        )
+        await this.getAllTasks()
     }
 }
 
