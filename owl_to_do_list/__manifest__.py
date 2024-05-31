cf47d8b4c1ffa2ae9c58034111633714232ef620
aarti-icreative <aarti@icreativetechnolabs.com>
2024-05-31 19:35:09 +0530

    [ADD]Inherit res partner list view button

--- owl_to_do_list/__manifest__.py
+++ owl_to_do_list/__manifest__.py
@@ -7,17 +7,18 @@
     'website': "https://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '17.0.1.0.0',
-    'depends': ['base'],
+    'depends': ['base','web'],
     "license": "LGPL-3",
     'data': [
         'security/ir.model.access.csv',
         'views/todo_list.xml',
+        'views/res_partner.xml',
     ],
     'assets':{
         'web.assets_backend': [
-            'owl_to_do_list/static/src/components/todo_list/todo_list.js',
-            'owl_to_do_list/static/src/components/todo_list/todo_list.xml',
-            'owl_to_do_list/static/src/components/todo_list/todo_list.scss',
+            'owl_to_do_list/static/src/components/*/*.js',
+            'owl_to_do_list/static/src/components/*/*.xml',
+            'owl_to_do_list/static/src/components/*/*.scss',
         ],
     }
 
