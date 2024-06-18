231a351c026428a6b46921e4572c96f58fd509ed
aarti-icreative <aarti@icreativetechnolabs.com>
2024-06-18 18:00:15 +0530

    [MOD]kanban view update

--- owl_to_do_list/static/src/components/view_inheritance/res_partner_kanban_view.js
+++ owl_to_do_list/static/src/components/view_inheritance/res_partner_kanban_view.js
@@ -5,29 +5,51 @@ import { kanbanView } from "@web/views/kanban/kanban_view"
 import { KanbanController } from "@web/views/kanban/kanban_controller"
 import { useService } from "@web/core/utils/hooks"
 
-class KanbanController extends KanbanController {
+const { onWillStart } = owl
+
+class ResPartnerKanbanController extends KanbanController {
     setup() {
         super.setup()
         this.action = useService("action")
+        this.orm = useService("orm")
+
+        onWillStart(async () => {
+            this.customerLocations = await this.orm.readGroup("res.partner", [], ['state_id'], ['state_id'])
+        })
     }
 
-    openSalesView(){
-        console.log("Open Sales view")
+    openSalesView() {
         this.action.doAction({
             type: "ir.actions.act_window",
             name: "Customer Sales",
             res_model: "sale.order",
-            views: [[false, "list"], [false, "form"]]
+            views: [
+                [false, "list"],
+                [false, "form"]
+            ]
         })
+    }
+
+    selectLocations(state) {
+
+        const id = state[0]
+        const name = state[1]
+
+        this.env.searchModel.createNewFilters([{
+            description: name,
+            domain: [
+                ['state_id', '=', id]
+            ],
+        }])
+
     }
 }
 
-
+ResPartnerKanbanController.template = "owl.ResPartnerKanbanView"
 
 export const resPartnerKanbanView = {
     ...kanbanView,
-    Controller: ResPartnerFormController,
-    button.template = "owl.ResPartnerKanbanView",
+    Controller: ResPartnerKanbanController,
 }
 
 registry.category("views").add("res_partner_kanban_view", resPartnerKanbanView)
